d0f28a0643ce1e22fd88e34b63bf1cbb37ec9625
Hafsa246 <shaikhhafsa25@gmail.com>
2023-07-09 18:48:33 +0530

    Add files via upload

---
+++ Day 6 - Functions (Reeborg's world)/Challenge 1 - Draw square.py
@@ -0,0 +1,22 @@
+#World :- Alone
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_around():
+    turn_left()
+    turn_left()
+    
+
+#Draw square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
